626a6234d09616c2b8f81242bfb700d56a31a6c0
beta86 <66570040+beta86@users.noreply.github.com>
2022-04-03 20:07:36 +0100

    Create new app with header and list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My List: </h1>
+    <ul>
+      <li> List item 1</li>
+      <li> List item 2</li>
+      <li> List item 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
